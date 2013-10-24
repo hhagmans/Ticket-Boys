83ed86d305e05526880312c9d73919b010a01bf6
hhagmans <hendrik.hagmans@web.de>
2013-10-24 16:25:28 +0200

    Basictest added

---
+++ src/test/java/de/fh_dortmund/ticket_system/Basistest.java
@@ -0,0 +1,14 @@
+package de.fh_dortmund.ticket_system;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Basistest {
+
+	@Test
+	public void test() {
+		assertEquals("Test","Test");
+	}
+
+}
