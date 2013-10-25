d233bfd067728e21331dc27036779075bd977018
hhagmans <hendrik.hagmans@web.de>
2013-10-25 14:11:10 +0200

    Changed test to hellobeantest

--- src/test/java/de/fh_dortmund/ticket_system/BasisTest.java
+++ src/test/java/de/fh_dortmund/ticket_system/BasisTest.java
@@ -2,13 +2,17 @@ package de.fh_dortmund.ticket_system;
 
 import static org.junit.Assert.*;
 
+import de.fh_dortmund.ticket_system.HelloBean;
+
 import org.junit.Test;
 
 public class BasisTest {
 
 	@Test
-	public void test() {
-		assertEquals("Test","Test");
+	public void testHelloBean() {
+		HelloBean hellobean = new HelloBean();
+		hellobean.setName("Test");
+		assert hellobean.getName() == "Test";
 	}
 
 }
