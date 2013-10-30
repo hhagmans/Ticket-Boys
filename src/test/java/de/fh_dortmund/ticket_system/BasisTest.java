67073d36e18cc30d89a025b605bd171ccca82727
Alex <alex.hof@live.de>
2013-10-30 12:21:45 +0100

    Css + template hinzugefügt.

--- src/test/java/de/fh_dortmund/ticket_system/BasisTest.java
+++ src/test/java/de/fh_dortmund/ticket_system/BasisTest.java
@@ -2,7 +2,7 @@ package de.fh_dortmund.ticket_system;
 
 import static org.junit.Assert.*;
 
-import de.fh_dortmund.ticket_system.HelloBean;
+import de.fh_dortmund.ticket_system.Login;
 
 import org.junit.Test;
 
@@ -10,7 +10,7 @@ public class BasisTest {
 
 	@Test
 	public void testHelloBean() {
-		HelloBean hellobean = new HelloBean();
+		Login hellobean = new Login();
 		hellobean.setName("Test");
 		assert hellobean.getName() == "Test";
 	}
