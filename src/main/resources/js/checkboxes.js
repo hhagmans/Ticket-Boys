f55e17217e85336bab374d20567740f2bfc76a8d
hhagmans <hendrik.hagmans@web.de>
2013-11-06 14:56:19 +0100

    js to prevent checking more than one shift

---
+++ src/main/resources/js/checkboxes.js
@@ -0,0 +1,25 @@
+$(document).ready( function() {
+	var toomuch = false;
+	var i = 0;
+$( ".ui-chkbox-box" ).click(function() {
+
+$( "tr[aria-selected='true'][role='row']" ).each(function() {
+	i++;
+});
+
+if (i >= 2) {
+	toomuch = true;
+}
+else {
+	toomuch = false;
+}
+	
+if (toomuch) {
+	$( "tr[aria-selected='false'][role='row']" ).removeClass("ui-datatable-selectable");
+}
+else {
+	$( "tr[role='row']" ).addClass("ui-datatable-selectable");
+}
+i = 0;
+});
+});
