2fbfb7325e8f2b82f557ff65287782a0f2045c01
Alex <alex.hof@live.de>
2013-11-06 13:12:20 +0100

    Imports...

--- src/main/java/de/fh_dortmund/ticket_system/entity/Employees.java
+++ src/main/java/de/fh_dortmund/ticket_system/entity/Employees.java
@@ -2,7 +2,6 @@ package de.fh_dortmund.ticket_system.entity;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Serializable;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,16 +9,10 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.model.ListDataModel;
 
-import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.ConversationScoped;
-import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.ViewAccessScoped;
 import org.primefaces.event.RowEditEvent;
-import org.primefaces.model.SelectableDataModel;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
