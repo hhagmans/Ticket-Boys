ca949a05f8895f03e1422691446c7d3833a2eafe
hhagmans <hendrik.hagmans@web.de>
2013-11-07 17:51:27 +0100

    Javascript working after shift change

--- src/main/webapp/resources/js/checkboxes.js
+++ src/main/webapp/resources/js/checkboxes.js
@@ -1,27 +1,26 @@
-$(document).ready( function() {
+$("body").on("click", ".ui-chkbox-box", function(){
+
 	var toomuch = false;
-	var i = 0;
-$( ".ui-chkbox-box" ).click(function() {
+	var i = 0;	
+		
+	$( "tr[aria-selected='true'][role='row']" ).each(function() {
+		i++;
+	});
 
-$( "tr[aria-selected='true'][role='row']" ).each(function() {
-	i++;
-});
-
-if (i >= 2) {
-	toomuch = true;
-}
-else {
-	toomuch = false;
-}
-	
-if (toomuch) {
-	$( "tr[aria-selected='false'][role='row']" ).removeClass("ui-datatable-selectable");
-	$( "tr[aria-selected='false'][role='row']" ).removeClass("ui-state-hover");
-	$( ".ui-chkbox-box" ).removeClass("ui-state-hover");
-}
-else {
-	$( "tr[role='row']" ).addClass("ui-datatable-selectable");
-}
-i = 0;
-});
-});
+	if (i >= 2) {
+		toomuch = true;
+	}
+	else {
+		toomuch = false;
+	}
+		
+	if (toomuch) {
+		$( "tr[aria-selected='false'][role='row']" ).removeClass("ui-datatable-selectable");
+		$( "tr[aria-selected='false'][role='row']" ).removeClass("ui-state-hover");
+		$( ".ui-chkbox-box" ).removeClass("ui-state-hover");
+	}
+	else {
+		$( "tr[role='row']" ).addClass("ui-datatable-selectable");
+	}
+	i = 0;
+	});
