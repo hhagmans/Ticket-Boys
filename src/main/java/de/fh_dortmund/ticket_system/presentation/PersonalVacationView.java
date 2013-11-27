e3200ede6152c229c519c22eeb823106a12f4f30
Timo <stimo@gmx.net>
2013-11-27 15:13:54 +0100

    fixed merge error

--- src/main/java/de/fh_dortmund/ticket_system/presentation/PersonalVacationView.java
+++ src/main/java/de/fh_dortmund/ticket_system/presentation/PersonalVacationView.java
@@ -18,7 +18,6 @@ import org.primefaces.model.ScheduleEvent;
 import org.primefaces.model.ScheduleModel;
 
 import de.fh_dortmund.ticket_system.business.PersonalVacationEventModel;
-import de.fh_dortmund.ticket_system.business.VacationEventModel;
 import de.fh_dortmund.ticket_system.entity.VacationEvent;
 import de.fh_dortmund.ticket_system.util.RightsManager;
 
