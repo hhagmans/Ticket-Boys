76bcb7505515c5fb7859c387f6173f1659a88497
mwienand <matthias.wienand@itemis.de>
2013-11-27 16:32:41 +0100

    Refactor findAllShifts: generate next year, arrange dispatchers only.

--- src/main/java/de/fh_dortmund/ticket_system/persistence/ShiftDaoTestImpl.java
+++ src/main/java/de/fh_dortmund/ticket_system/persistence/ShiftDaoTestImpl.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ApplicationScoped;
@@ -14,6 +15,7 @@ import com.google.gson.reflect.TypeToken;
 
 import de.fh_dortmund.ticket_system.business.ShiftCalculator;
 import de.fh_dortmund.ticket_system.entity.Employee;
+import de.fh_dortmund.ticket_system.entity.Role;
 import de.fh_dortmund.ticket_system.entity.Shift;
 
 @ManagedBean
@@ -22,23 +24,44 @@ public class ShiftDaoTestImpl implements ShiftDao, Serializable
 {
 	private static final long	serialVersionUID	= 1L;
 
+	/**
+	 * Fetches the list of dispatchers (employees with role =
+	 * {@link Role#dispatcher}). Calculates the shifts for those dispatchers
+	 * using {@link ShiftCalculator}.
+	 * 
+	 * @return a (generated) {@link List} of all {@link Shift}s, or
+	 *         <code>null</code> on error
+	 */
 	public List<Shift> findAllShifts()
 	{
-		List<Employee> empList = null;
-
-		empList = getEmployeeList();
+		List<Employee> dispatchers = getDispatchingEmployees();
 
 		// Verteilen der Employees auf die 52 "Shifts"
-		if (empList != null)
+		if (dispatchers != null)
 		{
-
 			ShiftCalculator sh = new ShiftCalculator();
-
-			return sh.generateDispatcherList(empList);
+			return sh.generateShiftList(dispatchers);
 		}
 
 		return null;
+	}
 
+	/**
+	 * Fetches the list of all employees and filters them by role. Returns all {@link Role#dispatcher}s.
+	 * 
+	 * @return all employees where role = {@link Role#dispatcher}
+	 * 
+	 * @see #getEmployeeList()
+	 */
+	private List<Employee> getDispatchingEmployees()
+	{
+		List<Employee> dispatchers = new ArrayList<Employee>();
+		
+		for (Employee e : getEmployeeList())
+			if (e.getRole().equals(Role.dispatcher))
+				dispatchers.add(e);
+		
+		return dispatchers;
 	}
 
 	private List<Employee> getEmployeeList()
