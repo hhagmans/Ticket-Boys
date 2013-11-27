dd36ed614c447d9f4a697c8d90eba6b382943a02
Stefan <stefan.wallkoetter@gmail.com>
2013-11-27 16:03:12 +0100

    checker in DailyChecker running scheduled tasks cron4j imported in pom.xml

--- src/main/java/de/fh_dortmund/ticket_system/util/DailyChecker.java
+++ src/main/java/de/fh_dortmund/ticket_system/util/DailyChecker.java
@@ -1,20 +1,20 @@
 package de.fh_dortmund.ticket_system.util;
 
+import it.sauronsoftware.cron4j.Scheduler;
+
 import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.faces.bean.ManagedProperty;
 
 import org.apache.commons.mail.EmailException;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.impl.StdSchedulerFactory;
 
 import de.fh_dortmund.ticket_system.business.EmployeeData;
 import de.fh_dortmund.ticket_system.business.ShiftData;
 import de.fh_dortmund.ticket_system.entity.Employee;
 import de.fh_dortmund.ticket_system.entity.Shift;
 import de.fh_dortmund.ticket_system.util.*;
+
 
 public class DailyChecker
 {	
@@ -60,40 +60,36 @@ public class DailyChecker
 	
 	public static void check(ShiftData shiftData)
 	{
-		/**
-		try
-		{
-
-			// Grab the Scheduler instance from the Factory
-			Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-
-			// and start it off
-			scheduler.start();
-			*/
-			try {
-				String msg = "Hallo " + getLatestEmployee(shiftData).getFullName() + ",\n\n"
-						+ "Sie sind in KW " + getLatestKW(shiftData) + " als Dispatcher eingetragen. Ihr Einsatz beginnt in Kürze.\n\nWeitere Details finden Sie unter http://localhost:8080/TicketSystem";
-				EmailUtil.sendEmail(msg, "ticketboys1337@gmail.com");
-			} catch (EmailException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			/**
-			try
-			{
-				Thread.sleep(1000000);
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-
-			scheduler.shutdown();
-		
+		final ShiftData newShiftData = shiftData;
+		// Creates a Scheduler instance.
+				Scheduler s = new Scheduler();
+				//schedule pattern for once a day at 8 AM
+				// "0 8 * * *"
+				// Schedule a once-a-minute task.
+				s.schedule("* * * * *", new Runnable() {
+					public void run() {
+						trigger(newShiftData);
+					}
+				});
+				// Starts the scheduler.
+				s.start();
+				// Will run for ten minutes.
+				try {
+					Thread.sleep(1000L * 60L * 10L);
+				} catch (InterruptedException e) {
+					;
+				}
+				// Stops the scheduler.
+				s.stop();
+	} 
+	
+	public static void trigger(ShiftData shiftData) {
+		try {
+			String msg = "Hallo " + getLatestEmployee(shiftData).getFullName() + ",\n\n" + "Sie sind in KW " + getLatestKW(shiftData) + " als Dispatcher eingetragen. Ihr Einsatz beginnt in Kürze.\n\nWeitere Details finden Sie unter http://localhost:8080/TicketSystem";
+			EmailUtil.sendEmail(msg, "ticketboys1337@gmail.com");
+		} catch (EmailException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
-		catch (SchedulerException se)
-		{
-			se.printStackTrace();
-		} */
 	}
 }
