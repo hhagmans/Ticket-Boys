86ab5685a476e9cf44599e4ccdcfbf8d0254e5c0
Timo <stimo@gmx.net>
2014-01-06 17:13:59 +0100

    fml.

--- src/main/java/de/fh_dortmund/ticket_system/business/ConflictData.java
+++ src/main/java/de/fh_dortmund/ticket_system/business/ConflictData.java
@@ -2,14 +2,12 @@ package de.fh_dortmund.ticket_system.business;
 
 import java.io.Serializable;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
 import de.fh_dortmund.ticket_system.base.BaseData;
 import de.fh_dortmund.ticket_system.entity.Conflict;
 import de.fh_dortmund.ticket_system.persistence.ConflictDaoSqlite;
-import de.fh_dortmund.ticket_system.util.TestdataProvider;
 
 @ManagedBean
 @ApplicationScoped
@@ -17,11 +15,11 @@ public class ConflictData extends BaseData<Conflict, ConflictDaoSqlite> implemen
 {
 	private static final long serialVersionUID = -836521223253264448L;
 
-	@PostConstruct
-	private void init()
-	{
-		TestdataProvider.fillConflict(getDao());
-	}
+	//	@PostConstruct
+	//	private void init()
+	//	{
+	//		TestdataProvider.fillConflict(getDao());
+	//	}
 
 	public ConflictData()
 	{
