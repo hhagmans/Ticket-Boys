3b3944c8c43660898eb569d3bc0130ddc2876863
Timo <stimo@gmx.net>
2014-01-07 16:29:29 +0100

    removed console outout of conflictData.findAll

--- src/main/java/de/fh_dortmund/ticket_system/persistence/ConflictDaoSqlite.java
+++ src/main/java/de/fh_dortmund/ticket_system/persistence/ConflictDaoSqlite.java
@@ -10,31 +10,31 @@ import de.fh_dortmund.ticket_system.base.BaseDaoSqlite;
 import de.fh_dortmund.ticket_system.entity.Conflict;
 import de.fh_dortmund.ticket_system.entity.Employee;
 
-public class ConflictDaoSqlite extends BaseDaoSqlite<Conflict> implements
-		ConflictDao, Serializable {
+public class ConflictDaoSqlite extends BaseDaoSqlite<Conflict> implements ConflictDao, Serializable
+{
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public List<Conflict> findAll() {
-		return getEm().createNamedQuery("Conflict_findAll", Conflict.class)
-				.getResultList();
+	public List<Conflict> findAll()
+	{
+		return getEm().createNamedQuery("Conflict_findAll", Conflict.class).getResultList();
 	}
 
-	public List<Conflict> findByUser(Employee emp) {
-		Query setParameter = getEm().createNamedQuery("Conflict_findAll",
-				Conflict.class);
+	@Override
+	public List<Conflict> findByUser(Employee emp)
+	{
+		Query setParameter = getEm().createNamedQuery("Conflict_findAll", Conflict.class);
 
-		ArrayList<Conflict> conflictList = new ArrayList<Conflict>(
-				setParameter.getResultList());
-
-		System.out.println("Find:");
-		System.out.println(conflictList);
+		ArrayList<Conflict> conflictList = new ArrayList<Conflict>(setParameter.getResultList());
 
 		List<Conflict> filteredList = new ArrayList<Conflict>();
-		for (Conflict conflict : conflictList) {
+		for (Conflict conflict : conflictList)
+		{
 			System.out.println(conflict);
 			if (!conflict.getSolved() && conflict.getEmployee().equals(emp))
+			{
 				filteredList.add(conflict);
+			}
 		}
 		return filteredList;
 	}
