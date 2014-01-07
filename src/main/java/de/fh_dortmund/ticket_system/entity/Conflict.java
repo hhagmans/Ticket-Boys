88185b957ebe9d5a401dbb0d7bdb5be7c2348853
hhagmans <hendrik.hagmans@web.de>
2014-01-07 12:17:19 +0100

    Conflict table gets created now it's possible to create conflicts

--- src/main/java/de/fh_dortmund/ticket_system/entity/Conflict.java
+++ src/main/java/de/fh_dortmund/ticket_system/entity/Conflict.java
@@ -3,7 +3,6 @@ package de.fh_dortmund.ticket_system.entity;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -17,65 +16,63 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "conflict")
-@NamedQueries({ @NamedQuery(name = "Conflict_findAll", query = "SELECT v FROM Conflict v"),
+@NamedQueries({
+		@NamedQuery(name = "Conflict_findAll", query = "SELECT v FROM Conflict v"),
 		@NamedQuery(name = "Conflict_findByUser", query = "SELECT v FROM Conflict v WHERE v.employee = :employee") })
-public class Conflict implements Serializable
-{
+public class Conflict implements Serializable {
+
 	private static final long serialVersionUID = 4982696674221907971L;
 
-	private Integer id;
+	private String id;
 	private Employee employee;
 	private Week week;
 	private Boolean solved;
 	private String name;
 
-	public Conflict(Employee employee, Week week)
-	{
+	public Conflict() {
+		this.id = null;
+	}
+
+	public Conflict(Employee employee, Week week) {
 		super();
 		this.employee = employee;
 		this.week = week;
 		this.solved = false;
 		this.name = "Kalenderwoche: " + week.getUniqueRowKey();
+		this.id = employee.getKonzernID() + week.getUniqueRowKey();
 	}
 
 	@Id
-	@GeneratedValue
-	public Integer getId()
-	{
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Integer id)
-	{
+	public void setId(String id) {
 		this.id = id;
 	}
 
-	public Employee getEmployee()
-	{
+	public Employee getEmployee() {
 		return employee;
 	}
 
-	public void setEmployee(Employee employee)
-	{
+	public void setEmployee(Employee employee) {
 		this.employee = employee;
 	}
 
-	public Week getWeek()
-	{
+	public Week getWeek() {
 		return week;
 	}
 
-	public void setWeek(Week week)
-	{
+	public void setWeek(Week week) {
 		this.week = week;
 	}
 
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = (prime * result) + ((employee == null) ? 0 : employee.hashCode());
+		result = (prime * result)
+				+ ((employee == null) ? 0 : employee.hashCode());
 		result = (prime * result) + ((id == null) ? 0 : id.hashCode());
 		result = (prime * result) + ((solved == null) ? 0 : solved.hashCode());
 		result = (prime * result) + ((week == null) ? 0 : week.hashCode());
@@ -83,96 +80,70 @@ public class Conflict implements Serializable
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return name;
 	}
 
 	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-		{
+	public boolean equals(Object obj) {
+		if (this == obj) {
 			return true;
 		}
-		if (obj == null)
-		{
+		if (obj == null) {
 			return false;
 		}
-		if (getClass() != obj.getClass())
-		{
+		if (getClass() != obj.getClass()) {
 			return false;
 		}
 		Conflict other = (Conflict) obj;
-		if (employee == null)
-		{
-			if (other.employee != null)
-			{
+		if (employee == null) {
+			if (other.employee != null) {
 				return false;
 			}
-		}
-		else if (!employee.equals(other.employee))
-		{
+		} else if (!employee.equals(other.employee)) {
 			return false;
 		}
-		if (id == null)
-		{
-			if (other.id != null)
-			{
+		if (id == null) {
+			if (other.id != null) {
 				return false;
 			}
-		}
-		else if (!id.equals(other.id))
-		{
+		} else if (!id.equals(other.id)) {
 			return false;
 		}
-		if (solved == null)
-		{
-			if (other.solved != null)
-			{
+		if (solved == null) {
+			if (other.solved != null) {
 				return false;
 			}
-		}
-		else if (!solved.equals(other.solved))
-		{
+		} else if (!solved.equals(other.solved)) {
 			return false;
 		}
-		if (week == null)
-		{
-			if (other.week != null)
-			{
+		if (week == null) {
+			if (other.week != null) {
 				return false;
 			}
-		}
-		else if (!week.equals(other.week))
-		{
+		} else if (!week.equals(other.week)) {
 			return false;
 		}
 		return true;
 	}
 
-	public Boolean getSolved()
-	{
+	public Boolean getSolved() {
 		return solved;
 	}
 
-	public void setSolved(Boolean solved)
-	{
+	public void setSolved(Boolean solved) {
 		this.solved = solved;
 
-		if ((solved == true) && !getName().substring(0, 9).equals("GELÖST - "))
-		{
+		if ((solved == true) && !getName().substring(0, 9).equals("GELÖST - ")) {
 			setName("GELÖST - " + getName());
 		}
 	}
 
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 }
--- src/main/java/de/fh_dortmund/ticket_system/persistence/ConflictDaoSqlite.java
+++ src/main/java/de/fh_dortmund/ticket_system/persistence/ConflictDaoSqlite.java
@@ -1,5 +1,6 @@
 package de.fh_dortmund.ticket_system.persistence;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Query;
@@ -8,18 +9,19 @@ import de.fh_dortmund.ticket_system.base.BaseDaoSqlite;
 import de.fh_dortmund.ticket_system.entity.Conflict;
 import de.fh_dortmund.ticket_system.entity.Employee;
 
-public class ConflictDaoSqlite extends BaseDaoSqlite<Conflict> implements ConflictDao
-{
+public class ConflictDaoSqlite extends BaseDaoSqlite<Conflict> implements
+		ConflictDao, Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Override
-	public List<Conflict> findAll()
-	{
-		return getEm().createNamedQuery("Conflict_findAll", Conflict.class).getResultList();
+	public List<Conflict> findAll() {
+		return getEm().createNamedQuery("Conflict_findAll", Conflict.class)
+				.getResultList();
 	}
 
-	public List<Conflict> findByUser(Employee emp)
-	{
-		Query setParameter = getEm().createNamedQuery("Conflict_findByUser", Conflict.class).setParameter("employee",
-			emp);
+	public List<Conflict> findByUser(Employee emp) {
+		Query setParameter = getEm().createNamedQuery("Conflict_findByUser",
+				Conflict.class).setParameter("employee", emp);
 		return setParameter.getResultList();
 	}
 
