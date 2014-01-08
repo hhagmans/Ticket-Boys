1b573368bd56f89da66a2de1fdcda5b5b8c552da
hhagmans <hendrik.hagmans@web.de>
2014-01-08 11:56:24 +0100

    Conflict solving working

--- src/main/java/de/fh_dortmund/ticket_system/presentation/ConflictView.java
+++ src/main/java/de/fh_dortmund/ticket_system/presentation/ConflictView.java
@@ -3,18 +3,24 @@ package de.fh_dortmund.ticket_system.presentation;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 
 import de.fh_dortmund.ticket_system.authentication.Authentication;
 import de.fh_dortmund.ticket_system.business.ConflictData;
+import de.fh_dortmund.ticket_system.business.ConflictModel;
 import de.fh_dortmund.ticket_system.entity.Conflict;
 import de.fh_dortmund.ticket_system.entity.Employee;
 
 @ManagedBean
 @ViewScoped
-public class ConflictView implements Serializable {
+public class ConflictView implements Serializable, Converter {
 
 	private static final long serialVersionUID = -6264092603228857048L;
 
@@ -26,17 +32,16 @@ public class ConflictView implements Serializable {
 
 	private Conflict selectedConflict;
 
+	private List<Conflict> selectedConflicts;
+
 	private List<Conflict> conflicts;
+
+	private ConflictModel conflictModel;
 
 	public List<Conflict> getUnsolvedConflictsForCurrentUser() {
 		Employee currentUser = auth.getEmployee();
 		List<Conflict> conflicts = conflictData.findByUser(currentUser);
 		List<Conflict> unsolvedConflicts = conflictData.findByUser(currentUser);
-		for (Conflict c : conflicts) {
-			if (!c.getSolved()) {
-				unsolvedConflicts.add(c);
-			}
-		}
 
 		return unsolvedConflicts;
 	}
@@ -51,19 +56,17 @@ public class ConflictView implements Serializable {
 
 	public void solveConflicts() {
 
-		Conflict conflict = getSelectedConflict();
-		if (conflict == null) {
-			return;
-		}
+		List<Conflict> conflicts = getSelectedConflicts();
 
-		if (conflict.getSolved()) {
-			return;
-		} else {
-			System.out.println("Conflict Name: " + conflict.getName()
-					+ " solved: " + conflict.getSolved());
-			conflict.setSolved(true);
-			conflict.setName("GELÖST: " + conflict.getName());
-			conflictData.update(conflict);
+		for (Conflict conflict : conflicts) {
+			if (conflict.getSolved()) {
+				return;
+
+			} else {
+				conflict.setSolved(true);
+				conflict.setName("GELÖST: " + conflict.getName());
+				conflictData.update(conflict);
+			}
 		}
 
 	}
@@ -76,6 +79,14 @@ public class ConflictView implements Serializable {
 		this.auth = auth;
 	}
 
+	public List<Conflict> getSelectedConflicts() {
+		return selectedConflicts;
+	}
+
+	public void setSelectedConflicts(List<Conflict> selectedConflicts) {
+		this.selectedConflicts = selectedConflicts;
+	}
+
 	public List<Conflict> getConflicts() {
 		return getUnsolvedConflictsForCurrentUser();
 	}
@@ -84,12 +95,63 @@ public class ConflictView implements Serializable {
 		this.conflicts = conflicts;
 	}
 
+	public ConflictModel getConflictModel() {
+		if (conflictModel == null)
+			setConflictModel(new ConflictModel(
+					getUnsolvedConflictsForCurrentUser()));
+
+		return conflictModel;
+	}
+
+	public void setConflictModel(ConflictModel conflictModel) {
+		this.conflictModel = conflictModel;
+	}
+
 	public Conflict getSelectedConflict() {
 		return selectedConflict;
 	}
 
 	public void setSelectedConflict(Conflict selectedConflict) {
 		this.selectedConflict = selectedConflict;
+	}
+
+	public Object getAsObject(FacesContext facesContext, UIComponent component,
+			String submittedValue) {
+		if (submittedValue.trim().equals("")) {
+			return null;
+		} else {
+			try {
+				String name = submittedValue;
+
+				ConflictData conflictDataclass = (ConflictData) facesContext
+						.getApplication()
+						.getELResolver()
+						.getValue(facesContext.getELContext(), null,
+								"conflictData");
+
+				for (Conflict c : conflictDataclass.findAll()) {
+					if (c.getName() == name) {
+						return c;
+					}
+				}
+
+			} catch (NumberFormatException exception) {
+				throw new ConverterException(new FacesMessage(
+						FacesMessage.SEVERITY_ERROR, "Conversion Error",
+						"Not a valid conflict"));
+			}
+		}
+
+		return null;
+	}
+
+	public String getAsString(FacesContext facesContext, UIComponent component,
+			Object value) {
+		if (value == null || value.equals("")) {
+			return "";
+		} else {
+			return String.valueOf(((Conflict) value).getName());
+		}
 	}
 
 }
