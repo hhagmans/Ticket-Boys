ea16f618b37ec5e8e1228528c87c99de6d52477f
stefanw138 <stefan.wallkoetter@gmail.com>
2014-01-29 14:13:42 +0100

    Class WebClientDevWrapper added to trust all certificates for SSL connection to LDAP service
    Authentication returns valid LDAP response string on given credentials

--- src/main/java/de/fh_dortmund/ticket_system/authentication/NoAuthentication.java
+++ src/main/java/de/fh_dortmund/ticket_system/authentication/NoAuthentication.java
@@ -1,7 +1,19 @@
 package de.fh_dortmund.ticket_system.authentication;
 
+import java.io.IOException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import de.fh_dortmund.ticket_system.util.WebClientDevWrapper;
+
 
 @ManagedBean(name = "auth")
 @SessionScoped
@@ -13,6 +25,42 @@ public class NoAuthentication extends Authentication {
 	 */
 	@Override
 	protected boolean authenticate(String name, String passwort) {
+		
+		String checkUrl = "https://bv.intranet.degussa.com/webservice/logincheck/loginCheckJson.cfc?method=checkUser&user=" + name + "&pwd=" + passwort + "&applikation=Tinnef&language=en";
+		
+//		trust all certificates for SSL connection
+		HttpClient httpclient = new DefaultHttpClient();
+		WebClientDevWrapper.wrapClient(httpclient);
+		
+        try {
+            HttpGet httpget = new HttpGet(checkUrl);
+            System.out.println("executing http request....");
+
+            // Create a response handler
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
+            // Body contains your json stirng
+            String responseBody = null;
+			try {
+				responseBody = httpclient.execute(httpget, responseHandler);
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+            System.out.println("----------------------------------------");
+
+        } finally {
+            // When HttpClient instance is no longer needed,
+            // shut down the connection manager to ensure
+            // immediate deallocation of all system resources
+            httpclient.getConnectionManager().shutdown();
+        }
+
+		
 		return true;
 	}
 
